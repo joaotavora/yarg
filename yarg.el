500dae760d313cba662ccc9ca93afddf2fbb0e8d
João Távora <joaotavora@gmail.com>
2026-05-04 13:03:38 +0100

    Support wgrep.el out of the box
    * yarg.el (yarg--colbeg, yarg--colend): yak-shave into shorter names
    (yarg-mode): Setup wgrep optionally.
    (yarg--re): new internal var.

--- yarg.el
+++ yarg.el
@@ -53,14 +53,13 @@
   (let* ((b (match-end 0)) (e (pos-eol b)) (m (text-property-any b e 'yarg t)))
     (and m (- (if endp (next-single-property-change m 'yarg nil e) m) b))))
 
-(defun yarg--colbeg () (yarg--col nil)) (defun yarg--colend () (yarg--col t))
+(defun yarg--cb () (yarg--col nil)) (defun yarg--ce () (yarg--col t))
 
+(defconst yarg--re "^\\(?1:.+?\\)\\(?::\\)\\(?3:[1-9][0-9]*\\)\\(?::\\)\\(?4:[1-9][0-9]*\\)\\(?::\\)")
 (defconst yarg-error-regexp-alist
   ;; rg --no-heading --column produces: file:line:col:content
   ;; Group 4 is the column number (always present with --column).
-  `(("^\\(.+?\\)\\(:\\)\\([1-9][0-9]*\\)\\2\
-\\(?:\\(?:\\(?4:[1-9][0-9]*\\)\\2\\)\\|[^0-9\n]\\|[0-9][^0-9\n]\\|\\.\\.\\.\\)"
-     1 3 (yarg--colbeg . yarg--colend) nil 1 (4 compilation-column-face nil t))
+  `((,yarg--re 1 3 (yarg--cb . yarg--ce) nil 1 (4 compilation-column-face nil t))
     ("^Binary file \\(.+\\) matches$" 1 nil nil 0 1))
   "Compilation error-regexp-alist for rg --no-heading --column output.")
 
@@ -68,7 +67,9 @@
   "Compilation mode for ripgrep output."
   (setq-local compilation-disable-input t)
   (setq-local compilation-error-face 'compilation-info)
-  (add-hook 'compilation-filter-hook #'yarg--filter nil t))
+  (add-hook 'compilation-filter-hook #'yarg--filter nil t)
+  (setq-local wgrep-line-file-regexp yarg--re)
+  (ignore-errors (with-no-warnings (wgrep-setup-internal))))
 
 (defvar yarg-history nil "Minibuffer history for `yarg'.")
 
